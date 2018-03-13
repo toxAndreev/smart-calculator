6ec214043d348aae756d1bb40b5703bfa5d75d11
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-03-13 17:03:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.initialValue=initialValue;
   }
 
   add(number) {
-    // your implementation
+  this.initialValue+='+'+number;
+  return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.initialValue+='-'+number;
+      return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.initialValue+='*'+number;
+      return this;
   }
 
   devide(number) {
-    // your implementation
+    this.initialValue+='/'+number;
+      return this;
   }
 
   pow(number) {
-    // your implementation
+    this.initialValue+='**'+number;
+      return this;
   }
+
+toString(){
+   return eval(this.initialValue);
+   }
 }
+
 
 module.exports = SmartCalculator;
